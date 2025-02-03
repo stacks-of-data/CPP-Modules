307fe06d7a5ee6080b6b36546cc8d44fc2d7700b
stacks-of-data <coder64bit@gmail.com>
2025-02-03 17:27:35 +0300

    fixes

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amsaleh <amsaleh@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 22:50:05 by amsaleh           #+#    #+#             */
-/*   Updated: 2025/02/03 15:57:36 by amsaleh          ###   ########.fr       */
+/*   Updated: 2025/02/03 17:27:25 by amsaleh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,5 @@ int main()
 	zombie->announce();
 	delete zombie;
 	randomChump("Brainz_randomChump");
-	for (int i = 1; i < 51; i++)
-	{
-		std::string z_name = "";
-		char	res[30];
-		if (std::sprintf(res, "Brainz_randomChump%d", i) == -1)
-		{
-			std::cerr << "An error occured in sprintf\n";
-			return (1);
-		}
-		z_name.append(res);
-		randomChump(z_name);
-	}
 	return (0);
 }
