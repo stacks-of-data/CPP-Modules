e759d157e8435e9ad845677e475eb4d5465b6e75
stacks-of-data <coder64bit@gmail.com>
2025-02-05 21:45:37 +0300

    fix

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amsaleh <amsaleh@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 00:56:26 by amsaleh           #+#    #+#             */
-/*   Updated: 2024/12/22 00:56:27 by amsaleh          ###   ########.fr       */
+/*   Updated: 2025/02/05 21:44:24 by amsaleh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ int main()
 	std::string*	stringPTR = &str;
 	std::string&	stringREF = str;
 
-	std::cout << "str memory address: " << (void *)stringREF.c_str() << '\n';
+	std::cout << "str memory address: " << &str << '\n';
 	std::cout << "stringPTR memory address: " << stringPTR << '\n';
-	std::cout << "stringREF memory address: " << (void *)stringREF.c_str() << '\n';
+	std::cout << "stringREF memory address: " << &stringREF << '\n';
 
 	std::cout << "str: " << str << '\n';
 	std::cout << "stringPTR: " << *stringPTR << '\n';
