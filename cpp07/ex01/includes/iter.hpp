7691c4356226e57536a4a39d47b3becb69fc44c1
stacks-of-data <coder64bit@gmail.com>
2025-05-06 22:11:53 +0300

    removed unused template

--- cpp07/ex01/includes/iter.hpp
+++ cpp07/ex01/includes/iter.hpp
@@ -6,14 +6,11 @@
 /*   By: amsaleh <amsaleh@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 17:04:49 by amsaleh           #+#    #+#             */
-/*   Updated: 2025/05/06 22:08:46 by amsaleh          ###   ########.fr       */
+/*   Updated: 2025/05/06 22:11:36 by amsaleh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cstdlib>
-
-template <typename T>
-void	func(T content);
 
 template <typename T, typename B>
 void	iter(T addr, size_t len, B func)
