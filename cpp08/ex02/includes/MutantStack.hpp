5e0cfc00b2139e724664fd0c0609d4dd59e147d9
stacks-of-data <coder64bit@gmail.com>
2025-05-08 03:14:47 +0300

    cpp08 ex02 MutantStack template class

--- cpp08/ex02/includes/MutantStack.hpp
+++ cpp08/ex02/includes/MutantStack.hpp
@@ -6,12 +6,63 @@
 /*   By: amsaleh <amsaleh@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 23:47:38 by amsaleh           #+#    #+#             */
-/*   Updated: 2025/05/07 23:48:42 by amsaleh          ###   ########.fr       */
+/*   Updated: 2025/05/08 03:11:52 by amsaleh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stack>
+
 template <class T>
-class MutantStack
+class MutantStack: public std::stack<T>
 {
-	
-}
+	public:
+	typedef typename std::deque<T>::iterator iterator;
+	typedef typename std::deque<T>::const_iterator const_iterator;
+	typedef typename std::deque<T>::reverse_iterator reverse_iterator;
+	typedef typename std::deque<T>::const_reverse_iterator const_reverse_iterator;
+	MutantStack(): std::stack<T>()
+	{
+	}
+	MutantStack(MutantStack& obj): std::stack<T>(obj)
+	{
+	}
+	~MutantStack()
+	{
+	}
+	MutantStack&	operator=(MutantStack& obj)
+	{
+		
+	}
+	iterator	begin()
+	{
+		return (this->c.begin());
+	}
+	const_iterator	cbegin() const
+	{
+		return (this->c.cbegin());
+	}
+	iterator	end()
+	{
+		return (this->c.end());
+	}
+	const_iterator	cend() const
+	{
+		return (this->c.cend());
+	}
+	reverse_iterator	rbegin()
+	{
+		return (this->c.rbegin());
+	}
+	const_reverse_iterator	crbegin() const
+	{
+		return (this->c.crbegin());
+	}
+	reverse_iterator	rend()
+	{
+		return (this->c.rend());
+	}
+	const_reverse_iterator	crend() const
+	{
+		return (this->c.crend());
+	}
+};
---
+++ cpp08/ex02/srcs/main.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amsaleh <amsaleh@student.42amman.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 01:52:41 by amsaleh           #+#    #+#             */
+/*   Updated: 2025/05/08 03:12:57 by amsaleh          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/MutantStack.hpp"
+#include <iostream>
+#include <iterator>
+
+int main()
+{
+MutantStack<int> mstack;
+mstack.push(5);
+mstack.push(17);
+std::cout << mstack.top() << std::endl;
+mstack.pop();
+std::cout << mstack.size() << std::endl;
+mstack.push(3);
+mstack.push(5);
+mstack.push(737);
+//[...]
+mstack.push(0);
+MutantStack<int>::iterator it = mstack.begin();
+MutantStack<int>::iterator ite = mstack.end();
+++it;
+--it;
+while (it != ite)
+{
+std::cout << *it << std::endl;
+++it;
+}
+std::stack<int> s(mstack);
+return 0;
+}
