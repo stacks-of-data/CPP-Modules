d8de8c0e2b6ddfaac64ab237d84d34cef96ae542
stacks-of-data <coder64bit@gmail.com>
2025-05-09 15:08:45 +0300

    usage of std::advance

--- cpp08/ex00/srcs/utils.cpp
+++ cpp08/ex00/srcs/utils.cpp
@@ -6,17 +6,18 @@
 /*   By: amsaleh <amsaleh@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 19:53:28 by amsaleh           #+#    #+#             */
-/*   Updated: 2025/05/07 19:53:50 by amsaleh          ###   ########.fr       */
+/*   Updated: 2025/05/09 15:08:10 by amsaleh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iterator>
+#include <list>
 #include "../includes/utils.hpp"
 
 int	getRandomListElement(std::list<int>& list)
 {
 	int iRand = rand() % list.size();
 	std::list<int>::iterator it = list.begin();
-	for (int i = 0; i < iRand; i++)
-		it++;
+	std::advance(it, iRand);
 	return (*it);
 }
